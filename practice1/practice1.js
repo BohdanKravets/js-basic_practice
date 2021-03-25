0f48233f74e1031dcc700db427a5552626d3d739
BohdanKravets <bohdan.kravets@hotmail.com>
2021-03-25 14:48:04 +0200

    practice1 update

--- practice1/practice1.js
+++ practice1/practice1.js
@@ -117,7 +117,7 @@ if(n1 <= n2 && n2 <= n3) {
     console.log(n2);
     console.log(n3);
 }
-else if(n1 < n2 && n1 < n3) {
+else if(n1 < n3 && n3 < n2) {
     console.log(n1);
     console.log(n3);
     console.log(n2);
